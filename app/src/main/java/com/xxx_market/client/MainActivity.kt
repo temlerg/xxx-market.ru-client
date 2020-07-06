8fe5c9d858422861daa1116c5d5a25871423ffc9
ArlanchikDrey <arlanchikdrey@yandex.ru>
2020-07-06 19:12:45 +0300

    first commit

---
+++ app/src/main/java/com/xxx_market/client/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xxx_market.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
